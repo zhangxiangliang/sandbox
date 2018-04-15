fc56eec2721c5883156629bd8451091a743a816e
ZHANGXIANGLIANG <326277403@qq.com>
2018-04-16 01:10:51 +0800

    [控制器] Checkout 控制器 添加微信支付需要的数据到 render 方法中

--- app/Controllers/Http/CheckoutController.js
+++ app/Controllers/Http/CheckoutController.js
@@ -1,7 +1,27 @@
 'use strict'
 
+const Config = use('Config')
+const moment = use('moment')
+
 class CheckoutController {
+  /**
+   * @param out_trade_no 商户订单号
+   * @param body 商品描述
+   * @param total_fee 商品价格（单位：分）
+   * @param trade_type 支付类型
+   * @param product_id 商品 ID
+   */
   render ({ view }) {
+    const appid = Config.get('wxpay.appid')
+    const mch_id = Config.get('wxpay.mch_id')
+    const key = Config.get('wxpay.key')
+
+    const out_trade_no = moment().local().format('YYYYMMDDHHmmss')
+    const body = 'adonis'
+    const total_fee = 3
+    const trade_type = "NATIVE"
+    const product_id = 1
+
     return view.render('commerce.checkout')
   }
 }
