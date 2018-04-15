de90373b1d06dd6b2e6888ad131c27cc7e9faaae
ZHANGXIANGLIANG <326277403@qq.com>
2018-04-16 01:46:55 +0800

    [控制器] checkout 控制器 增加 _signGenerate 方法 根据订单数据 生成 sign 签名

--- app/Controllers/Http/CheckoutController.js
+++ app/Controllers/Http/CheckoutController.js
@@ -2,6 +2,10 @@
 
 const Config = use('Config')
 const moment = use('moment')
+const randomString = use('randomstring')
+const logger = use('App/Services/Logger')
+const queryString = use('querystring')
+const crypto = use('crypto')
 
 class CheckoutController {
   /**
@@ -15,6 +19,7 @@ class CheckoutController {
    * @param total_fee 商品价格（单位：分）
    * @param trade_type 支付类型
    * @param product_id 商品 ID
+   *
    */
   render ({ view }) {
     const appid = Config.get('wxpay.appid')
@@ -27,12 +32,51 @@ class CheckoutController {
     const total_fee = 3
     const trade_type = "NATIVE"
     const product_id = 1
+    const nonce_str = randomString.generate(32)
+
+    let sign = this._signGenerate({
+      appid, mch_id, notify_url,
+      out_trade_no, nonce_str, total_fee,
+      trade_type, product_id, key, body,
+    })
+
+    logger.debug(sign)
 
     return view.render('commerce.checkout')
-  },
+  }
 
   wxPayNotify () {
 
+  }
+
+  /**
+   * @description
+   * 1. sort 对数据安装字段名进行排序
+   * 2. querystring 对数据进行格式转换 key=value&
+   * 3. key 在 querystring 最后加上微信后台设置的 key
+   * 4. md5 加密
+   * 5. toUpperCase 把数据格式化为大写
+   */
+  _signGenerate(order) {
+    const key = order.key
+
+    const sortedOrder = Object.keys(order).sort()
+      .filter(item => item != 'key')
+      .reduce((accumulator, key) => {
+        accumulator[key] = order[key]
+        return accumulator
+      }, {})
+
+    const stringOrder = queryString.stringify(sortedOrder, null, null, { encodeURIComponent: queryString.unescape })
+
+    const stringOrderWithKey = `${ stringOrder }&key=${ key }`
+
+    const sign = crypto.createHash('md5')
+      .update(stringOrderWithKey)
+      .digest('hex')
+      .toUpperCase()
+
+    return sign
   }
 }
 
